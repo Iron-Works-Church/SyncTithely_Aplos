a3f46b3e3e83ddab0fe6b40eaead020497d837b2
Darin Pesnell <darin@ironworkschurch.org>
2020-10-12 09:08:21 -0400

    add church planting transfers

--- aplos.py
+++ aplos.py
@@ -5,6 +5,8 @@ import os
 import textwrap
 import subprocess
 import sys
+import boto3
+import pprint
 
 try:
     from requests import requests
@@ -18,10 +20,21 @@ except:
 
 api_base_url = 'https://www.aplos.com/hermes/api/v1/'
 
+sns = boto3.client('sns')
 
 with open("creds.json", encoding='utf-8') as f:
     credentials = json.load(f)
+    sc_user = credentials["sc_user"]
+    sc_pass = credentials["sc_pass"]
+    sc_baseurl = credentials["sc_baseurl"]
+    s3_bucket = credentials["s3_bucket"]
+    sns_topic = credentials["sns_topic"]
     api_id = credentials["aplos_api_id"]
+    api_base_url = credentials["api_base_url"]
+    tithely_user = credentials["tithely_user"]
+    tithely_pass = credentials["tithely_pass"]
+    tithely_org = credentials["tithely_org"]
+    church_name = credentials["church_name"]
 
 
 with open(api_id + '.key', mode='rb') as pkeyfile:
@@ -88,7 +101,120 @@ def api_transactions_get(api_base_url, api_id, api_access_token, parameters):
     #print(response)
     return (response)
 
+def add_deposit_aplos(api_base_url, api_id, api_access_token, batch_details, church_name):
+    headers = {'Authorization': 'Bearer: {}'.format(api_access_token)}
+    print('Posting URL: {}transactions'.format(api_base_url))
+    print('With headers: {}'.format(headers))
+    line_num = 0
+    payload = {
+  "note": batch_details["name"],
+  "date": batch_details["date"],
+  "contact": {
+     "companyname": church_name,
+     "type": "company"
+    },
+    "lines": []
+    }
+    for k, v in batch_details["details"].items():
+        payload["lines"].append({"amount": v["amount"], "account": {"account_number": 1000}, "fund": {"id": v["id"]}})
+        payload["lines"].append({"amount": 0 - v["amount"], "account": {"account_number": 4002}, "fund": {"id": v["id"]}})
+        payload["lines"].append({"amount": v["fees"], "account": {"account_number": 5003}, "fund": {"id": v["id"]}})
+        payload["lines"].append({"amount": 0 - v["fees"], "account": {"account_number": 1000}, "fund": {"id": v["id"]}})
+    jsonData = json.dumps(payload)
+    r = requests.post(
+        '{}transactions'.format(api_base_url), headers=headers, data=jsonData)
+    api_error_handling(r.status_code)
+    response = r.json()
+    sns.publish(TopicArn=sns_topic, Message=('JSON response: {}'.format(response)))
 
+def create_cp_xfer_expense(api_base_url, api_id, api_access_token, batch_details, church_name):
+    headers = {'Authorization': 'Bearer: {}'.format(api_access_token)}
+    print('Posting URL: {}transactions'.format(api_base_url))
+    print('With headers: {}'.format(headers))
+    line_num = 0
+    payload = {
+  "note": "Church Planting Transfer from General (Automated)",
+  "date": batch_details["date"],
+  "contact": {
+     "companyname": church_name,
+     "type": "company"
+    },
+    "lines": []
+    }
+    for k, v in batch_details["details"].items():
+        if v["id"] == 48655:
+            xfer_amount = v["amount"] / 10
+            xfer_amount = round(xfer_amount, 2)
+            payload["lines"].append({"amount": xfer_amount, "account": {"account_number": 16000}, "fund": {"id": v["id"]}})
+            payload["lines"].append({"amount": 0 - xfer_amount, "account": {"account_number": 1000}, "fund": {"id": v["id"]}})
+         
+    jsonData = json.dumps(payload)
+    r = requests.post(
+        '{}transactions'.format(api_base_url), headers=headers, data=jsonData)
+    api_error_handling(r.status_code)
+    response = r.json()
+    sns.publish(TopicArn=sns_topic, Message=('JSON response: {}'.format(response)))
+
+def create_cp_xfer_deposit(api_base_url, api_id, api_access_token, batch_details, church_name):
+    headers = {'Authorization': 'Bearer: {}'.format(api_access_token)}
+    print('Posting URL: {}transactions'.format(api_base_url))
+    print('With headers: {}'.format(headers))
+    line_num = 0
+    payload = {
+  "note": "Church Planting Deposit to CP Fund (Automated)",
+  "date": batch_details["date"],
+  "contact": {
+     "companyname": church_name,
+     "type": "company"
+    },
+    "lines": []
+    }
+    for k, v in batch_details["details"].items():
+        xfer_amount = v["amount"] / 10
+        xfer_amount = round(xfer_amount, 2)
+        #only transfer money for that received in general fund - note that funds are different for the deposit side
+        if v["id"] == 48655:
+            payload["lines"].append({"amount": xfer_amount, "account": {"account_number": 1000}, "fund": {"id": 50194}})
+            payload["lines"].append({"amount": 0 - xfer_amount, "account": {"account_number": 4251}, "fund": {"id": 50194}})
+         
+    jsonData = json.dumps(payload)
+    r = requests.post(
+        '{}transactions'.format(api_base_url), headers=headers, data=jsonData)
+    api_error_handling(r.status_code)
+    response = r.json()
+    sns.publish(TopicArn=sns_topic, Message=('JSON response: {}'.format(response)))
+
+def check_aplos(batch_details):
+    params = {}
+    params["f_rangestart"] = batch_details["date"]
+    params["f_rangeend"] = batch_details["date"]
+    aplos_transactions = api_transactions_get(api_base_url, api_id, api_access_token, params)
+    deposit_exists = False
+    for i in aplos_transactions["data"]["transactions"]:
+        if i["note"] == batch_details["name"] and "${:,.2f}".format(i["amount"]) == "${:,.2f}".format(float(batch_details["total"])):
+            deposit_exists = True
+    if deposit_exists:
+        print("Deposit Exists - No action Taken")
+    else:
+        print("Making new Deposit")
+    return(deposit_exists)
+
+def match_funds(api_base_url, api_id, api_access_token, batch_details):
+    headers = {'Authorization': 'Bearer: {}'.format(api_access_token)}
+    print('geting URL: {}funds'.format(api_base_url))
+    print('With headers: {}'.format(headers))
+    r = requests.get('{}funds'.format(api_base_url), headers=headers)
+    api_error_handling(r.status_code) 
+    response1 = {}
+    response1 = r.json()
+    for i, v in batch_details["details"].items():
+        for i2 in response1["data"]["funds"]:
+            if i == i2["name"]:
+                v["id"] = i2["id"]
+        if v["id"] == 62:
+            sns.publish(TopicArn=sns_topic, Message="fund " + i + " does not match anything in aplos")
+            quit()
+    return(batch_details)
 
 api_access_token = api_auth(api_base_url, api_id, api_user_key)
 
--- lambda_function.py
+++ lambda_function.py
@@ -8,7 +8,6 @@ import boto3
 
 sns = boto3.client('sns')
 
-
 with open("creds.json", encoding='utf-8') as f:
     credentials = json.load(f)
     sc_user = credentials["sc_user"]
@@ -61,23 +60,6 @@ def find_batch_details(response, deposit_date, charge_type):
                 batch_details["details"][fund]["fees"] = int(batch_details["details"][fund]["fees"]) + int(i["fees"])
     return(batch_details)
 
-def match_funds(api_base_url, api_id, api_access_token, batch_details):
-    headers = {'Authorization': 'Bearer: {}'.format(api_access_token)}
-    print('geting URL: {}funds'.format(api_base_url))
-    print('With headers: {}'.format(headers))
-    r = requests.get('{}funds'.format(api_base_url), headers=headers)
-    api_error_handling(r.status_code) 
-    response1 = {}
-    response1 = r.json()
-    for i, v in batch_details["details"].items():
-        for i2 in response1["data"]["funds"]:
-            if i == i2["name"]:
-                v["id"] = i2["id"]
-        if v["id"] == 62:
-            sns.publish(TopicArn=sns_topic, Message="fund " + i + " does not match anything in aplos")
-            quit()
-    return(batch_details)
-
 def convert_cents_dollars_find_total(batch_details):
     batch_details["total"] = 0
     for i, v in batch_details["details"].items():
@@ -86,47 +68,6 @@ def convert_cents_dollars_find_total(batch_details):
        batch_details["total"] = v["amount"] + batch_details["total"] + v["fees"]
        batch_details["total"] = round(batch_details["total"], 2)
     return(batch_details)
-
-def add_deposit_aplos(api_base_url, api_id, api_access_token, batch_details):
-    headers = {'Authorization': 'Bearer: {}'.format(api_access_token)}
-    print('Posting URL: {}transactions'.format(api_base_url))
-    print('With headers: {}'.format(headers))
-    line_num = 0
-    payload = {
-  "note": batch_details["name"],
-  "date": batch_details["date"],
-  "contact": {
-     "companyname": church_name,
-     "type": "company"
-    },
-    "lines": []
-    }
-    for k, v in batch_details["details"].items():
-        payload["lines"].append({"amount": v["amount"], "account": {"account_number": 1000}, "fund": {"id": v["id"]}})
-        payload["lines"].append({"amount": 0 - v["amount"], "account": {"account_number": 4002}, "fund": {"id": v["id"]}})
-        payload["lines"].append({"amount": v["fees"], "account": {"account_number": 5003}, "fund": {"id": v["id"]}})
-        payload["lines"].append({"amount": 0 - v["fees"], "account": {"account_number": 1000}, "fund": {"id": v["id"]}})
-    jsonData = json.dumps(payload)
-    r = requests.post(
-        '{}transactions'.format(api_base_url), headers=headers, data=jsonData)
-    api_error_handling(r.status_code)
-    response = r.json()
-    sns.publish(TopicArn=sns_topic, Message=('JSON response: {}'.format(response)))
-
-def check_aplos(batch_details):
-    params = {}
-    params["f_rangestart"] = batch_details["date"]
-    params["f_rangeend"] = batch_details["date"]
-    aplos_transactions = api_transactions_get(api_base_url, api_id, api_access_token, params)
-    deposit_exists = False
-    for i in aplos_transactions["data"]["transactions"]:
-        if i["note"] == batch_details["name"] and "${:,.2f}".format(i["amount"]) == "${:,.2f}".format(float(batch_details["total"])):
-            deposit_exists = True
-    if deposit_exists:
-        print("Deposit Exists - No action Taken")
-    else:
-        print("Making new Deposit")
-    return(deposit_exists)
 
 def get_tithely_method(tithely_org, tithely_user, tithely_pass, charge_id):
     parameters = {"organization_id": tithely_org, "limit": 100}
@@ -140,9 +81,9 @@ def process_transactions(charge_type, response, deposit_date):
     batch_details = convert_cents_dollars_find_total(batch_details)
     deposit_exists = check_aplos(batch_details)
     if not deposit_exists:
-        add_deposit_aplos(api_base_url, api_id, api_access_token, batch_details)
-
-
+        add_deposit_aplos(api_base_url, api_id, api_access_token, batch_details, church_name)
+        create_cp_xfer_expense(api_base_url, api_id, api_access_token, batch_details, church_name)
+        create_cp_xfer_deposit(api_base_url, api_id, api_access_token, batch_details, church_name)
 
 def lambda_handler(event, context):
     # Initial Query
@@ -167,5 +108,3 @@ def lambda_handler(event, context):
     }
 
 
-
-lambda_handler("test", "test")
